ba25cf572a5c2ac13470a0faf215629c1144fc73
Ryo Kanbayashi <ryo.contact@gmail.com>
2019-03-04 06:36:05 +0900

    new yv12 convertion not occur simasima. but color is not collect totally

--- PixFormatConvertTest/Program.cs
+++ PixFormatConvertTest/Program.cs
@@ -8,8 +8,16 @@ namespace PixFormatConvertTest
         static void Main(string[] args)
         {
             byte[] yuv_arr = Utils.readByteArrayFromFile("/Users/ryo/work/orylab/devenv/RemoteDesktopOneWindowForNovelGrame/scr_capture_yum420planner_yv12_1363x765.raw");
-            byte[] rgba8888_arr = Utils.YV12ToRGBA8888(yuv_arr, 1363, 765);
+            //byte[] yuv_arr = Utils.readByteArrayFromFile("/Users/ryo/work/orylab/devenv/RemoteDesktopOneWindowForNovelGrame/tulips_yuv420_prog_planar_qcif_176x144.yuv");
+            //byte[] rgba8888_arr = Utils.YV12ToRGBA8888(yuv_arr, 1363, 765);
+            byte[] rgba8888_arr = Utils.YV12ToRGBA8888_2(yuv_arr, 1363, 765);
+            //byte[] rgba8888_arr = Utils.YV12ToRGBA8888_Fast(yuv_arr, 1363, 765);
+            //byte[] rgba8888_arr = Utils.YV12ToRGBA8888(yuv_arr, 765, 1363);
+            //byte[] rgba8888_arr = Utils.YV12ToRGBA8888(yuv_arr, 176, 144);
+            //byte[] rgba8888_arr = Utils.YV12ToRGBA8888_Fast(yuv_arr, 176, 144);
             Utils.saveByteArrayToFile(rgba8888_arr, "/Users/ryo/work/orylab/devenv/RemoteDesktopOneWindowForNovelGrame/scr_capture_rgba8888_1363x765.raw");
+            //Utils.saveByteArrayToFile(rgba8888_arr, "/Users/ryo/work/orylab/devenv/RemoteDesktopOneWindowForNovelGrame/tulips_yuv420_prog_planar_qcif_176x144.raw");
+
             Console.WriteLine("convert finish!");
         }
     }
