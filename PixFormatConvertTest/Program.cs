3ef274beb5dd6e0d424cd5b168bacac74716140e
Ryo Kanbayashi <ryo.contact@gmail.com>
2019-03-04 19:41:17 +0900

    converting of yuv420 planar (yv12) to rgba8888 worked collectly

--- PixFormatConvertTest/Program.cs
+++ PixFormatConvertTest/Program.cs
@@ -7,15 +7,15 @@ namespace PixFormatConvertTest
     {
         static void Main(string[] args)
         {
-            byte[] yuv_arr = Utils.readByteArrayFromFile("/Users/ryo/work/orylab/devenv/RemoteDesktopOneWindowForNovelGrame/scr_capture_yum420planner_yv12_1363x765.raw");
+            byte[] yuv_arr = Utils.readByteArrayFromFile("/Users/ryo/work/orylab/devenv/RemoteDesktopOneWindowForNovelGrame/scr_capture_yum420planner_swapuv_yv12_1318x692.yuv");
             //byte[] yuv_arr = Utils.readByteArrayFromFile("/Users/ryo/work/orylab/devenv/RemoteDesktopOneWindowForNovelGrame/tulips_yvu420_inter_planar_qcif_real_yv12_poi_176x144.yuv");
             //byte[] rgba8888_arr = Utils.YV12ToRGBA8888(yuv_arr, 1363, 765);
-            byte[] rgba8888_arr = Utils.YV12ToRGBA8888_3(yuv_arr, 1363, 765);
+            byte[] rgba8888_arr = Utils.YV12ToRGBA8888(yuv_arr, 1318, 692);
             //byte[] rgba8888_arr = Utils.YV12ToRGBA8888_Fast(yuv_arr, 1363, 765);
             //byte[] rgba8888_arr = Utils.YV12ToRGBA8888(yuv_arr, 765, 1363);
             //byte[] rgba8888_arr = Utils.YV12ToRGBA8888(yuv_arr, 176, 144);
             //byte[] rgba8888_arr = Utils.YV12ToRGBA8888_Fast(yuv_arr, 176, 144);
-            Utils.saveByteArrayToFile(rgba8888_arr, "/Users/ryo/work/orylab/devenv/RemoteDesktopOneWindowForNovelGrame/scr_capture_rgba8888_1363x765.raw");
+            Utils.saveByteArrayToFile(rgba8888_arr, "/Users/ryo/work/orylab/devenv/RemoteDesktopOneWindowForNovelGrame/scr_capture_rgba8888_fromo_swapuv_yv12_1318x692.raw");
             //Utils.saveByteArrayToFile(rgba8888_arr, "/Users/ryo/work/orylab/devenv/RemoteDesktopOneWindowForNovelGrame/tulips_yvu420_inter_planar_qcif_real_yv12_poi_176x144_rgba8888.raw");
 
             Console.WriteLine("convert finish!");
